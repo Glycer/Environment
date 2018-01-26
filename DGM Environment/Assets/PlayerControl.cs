99c14d199d83db04c244c9eb37a0ce64a11720aa
Unknown <hopskipbye@gmail.com>
2018-01-26 00:25:50 -0700

    Project Upload

---
+++ DGM Environment/Assets/PlayerControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float speed;
+
+    CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        controller.SimpleMove(Input.GetAxis("Vertical") * transform.forward * speed);
+
+        transform.Rotate(0, Input.GetAxis("Horizontal") * 2, 0);
+    }
+}
---
+++ DGM Environment/Assets/ViewControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewControl : MonoBehaviour {
+
+    float delay = .03f;
+
+    Coroutine follow;
+    bool isFollowing = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //StartFollow();
+    }
+
+    private void Update()
+    {
+        /*
+        if (Input.GetKeyDown(KeyCode.Escape) && isFollowing)
+        {
+            StopFollow();
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && !isFollowing)
+        {
+            StartFollow();
+        }
+        */
+    }
+
+    IEnumerator Follow()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+        }
+    }
+
+    void StartFollow()
+    {
+        follow = StartCoroutine(Follow());
+        isFollowing = true;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void StopFollow()
+    {
+        StopCoroutine(follow);
+        isFollowing = false;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+}
