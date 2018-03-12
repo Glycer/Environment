161728625be03d2c55e7e04ff3c995e8d75831ff
Unknown <hopskipbye@gmail.com>
2018-03-12 14:26:08 -0600

    Lighting Edits

---
+++ DGM Environment/Assets/Scripts/LiftControl.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiftControl : MonoBehaviour {
+
+    public static Action<Transform> Lift;
+    public static Action<Transform> LetGo;
+    public Transform effect;
+
+    Transform storedParent;
+
+	// Use this for initialization
+	void Start () {
+        Lift += Pull;
+        LetGo += Drop;
+	}
+
+    void Pull(Transform obj)
+    {
+        //effect.gameObject.SetActive(true);
+
+        storedParent = obj.parent;
+
+        obj.position = transform.position;
+        obj.parent = transform;
+        LockPosition(obj, true);
+    }
+
+    void Drop(Transform obj)
+    {
+        //effect.gameObject.SetActive(false);
+
+        obj.parent = storedParent;
+        LockPosition(obj, false);
+    }
+
+    void LockPosition(Transform obj, bool isLock)
+    {
+        Rigidbody rigid = obj.GetComponent<Rigidbody>();
+        rigid.constraints = isLock ? RigidbodyConstraints.FreezeAll : RigidbodyConstraints.None;
+    }
+}
---
+++ DGM Environment/Assets/Scripts/Liftable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Liftable : MonoBehaviour {
+
+    Interactable interact;
+
+	// Use this for initialization
+	void Start () {
+        interact = GetComponent<Interactable>();
+
+        interact.Activate += Controlled;
+	}
+
+    void Controlled(bool isControlled)
+    {
+        if (isControlled)
+            LiftControl.Lift(transform);
+        else
+            LiftControl.LetGo(transform);
+    }
+}
